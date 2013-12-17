cfadca5f00aea0035940de65f9ea18b1cd744392
Scott Li <scottli0101@gmail.com>
2013-12-17 15:10:21 -0800

    begun tweet parser

--- app/services/tweet_parser.rb
+++ app/services/tweet_parser.rb
@@ -1,19 +1,57 @@
 class TweetParser
 
-  attr_accessor :tweet, :values
+  attr_accessor :tweet, :values, :recipients, :amounts
 
-  def initialize(tweet)
+  BOT = "@tippercoin"
+
+  def initialize(tweet, sender)
     @tweet = tweet
+    @recipients = Extractor::Recipients.parse(@tweet)
+    @amounts = Extractor::Amounts.parse(@tweet)
 
-    @values = build_values
+    @values = {
+      recipient: @recipients.first,
+      amount: @amounts.first,
+      sender: sender
+    }
   end
 
-  def build_values
-    return {
-      recipient: "",
-      sender: "",
-      amount: 1
-    }
+  def valid?
+    !@values.values.include?(nil)
+  end
+
+  def multiple_recipients?
+    @recipients.count > 1
+  end
+
+  module Extractor
+    module Recipients
+      extend self
+
+      # Accept: String
+      # Returns: Array or Strings, or nil
+      def parse(tweet)
+        usernames = tweet.scan(/(@\w+)/).flatten
+        usernames.delete(BOT)
+        return [nil] if usernames.blank?
+        return usernames
+      end
+
+    end
+
+    module Amounts
+      extend self
+
+      ### Supported Currency Symbols:
+      # ...
+
+      # Accept: String
+      # Returns: Array of Integers, or nil
+      def parse(tweet)
+        return [nil]
+      end
+
+    end
   end
 
 end
--- spec/tweet_parser.rb
+++ spec/tweet_parser.rb
@@ -1,17 +1,87 @@
 require 'spec_helper'
 
 describe 'TweetParser Unit Check' do
+  let(:sender) { "@sender" }
 
-  before(:each) do
+  context "Basic" do
+
+    before(:each) do
+      @tweet = "@recipient, really good article, keep it up! Here's a tip 0.001 BTC @tippercoin"
+      @t = TweetParser.new(@tweet, sender)
+    end
+
+    it "should return the correct sender" do
+      expect(@t.values[:sender]).to eq("@sender")
+    end
+
+    it "should return the correct value"
+
+    it "should return the correct bot(@tippercoin)" do
+      expect(TweetParser::BOT).to eq("@tippercoin")
+    end
+
+    it "should not return recipient to be @tippercoin" do
+      expect(@t.recipients).to_not include("@tippercoin")
+    end
+
+    it "should return recipients as array of strings or nil"
+
+    it "should return amounts as array of integers or nil"
+
+    it "should build a values hash"
 
   end
 
-  it "should" do
-    expect(false).to eq(true)
+  context "Multiple Recipients" do
+
+    before(:each) do
+      @tweet = "@recipient1, @recipient2, really good article, keep it up! Here's a tip 0.001 BTC @tippercoin"
+      @t = TweetParser.new(@tweet, sender)
+    end
+
+    it "should identify multiple recipients" do
+      expect(@t.multiple_recipients?).to eq(true)
+    end
+
+    it "should take the first recipient to be the real recipient" do
+      expect(@t.values[:recipient]).to eq("@recipient1")
+      expect(@t.values[:recipient]).to_not eq("@recipient2")
+    end
   end
 
-  it "should" do
-    expect(true).to eq(true)
+
+  context "Currency Symbols" do
+
+
+    it "should extract/convert $ to SATOSHIS"
+
+    it "should extract/convert BTC to SATOSHIS"
+
+    it "should extract/convert mBTC to SATOSHIS"
+
+    it "should extract/convert ฿ to SATOSHIS"
+
+    it "should extract/convert B to SATOSHIS"
+
+    it "should extract/convert beers to SATOSHIS"
+
+    it "should extract/convert internets to SATOSHIS"
+
+    it "should be extract both upper/lowercases on BTC/btc"
+
+    it "should prefer btc/BTC if multiple currency symbols"
+
+    it "should extract amounts with/without spaces"
+
+  end
+
+  context "Invalid tweets" do
+    it "should contain an invalid attribute for invalid tweets"
+
+    it "should be invalid if amount is zero"
+
+    it "should be invalid if BOT is specified as first recipient"
+
   end
 
 
