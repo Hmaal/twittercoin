17bf7a98bf174fee516b0a0fe45a8da167dd7bdb
Scott Li <scottli0101@gmail.com>
2013-12-18 15:39:38 -0800

    added pusher

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,5 @@
+# Pusher.url = "http://#{ENV["PUSHER_KEY"]}:#{ENV["PUSHER_SECRET"]}@api.pusherapp.com/apps/#{ENV["PUSHER_APP_ID"]}"
+
+Pusher.app_id = ENV["PUSHER_APP_ID"]
+Pusher.key = ENV["PUSHER_KEY"]
+Pusher.secret = ENV["PUSHER_SECRET"]
