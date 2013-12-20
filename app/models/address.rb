6ea14bd0fe985a502a4911365d1d2df2924ffe84
Scott Li <scottli0101@gmail.com>
2013-12-19 19:00:20 -0800

    added more model validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,8 @@ class Address < ActiveRecord::Base
 
   has_many :transactions
 
+  validates :encrypted_private_key, presence: true, uniqueness: true
+  validates :public_key, presence: true, uniqueness: true
+  validates :user_id, presence: true
+
 end
