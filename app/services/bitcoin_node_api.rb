d98c975e3ff6d5802d861938ec0ffc25454c63cc
sidazhang <sidazhang89@gmail.com>
2013-12-20 01:15:46 -0800

    Blockchain Info API wrapper

--- app/services/bitcoin_node_api.rb
+++ app/services/bitcoin_node_api.rb
@@ -1,8 +1,30 @@
 module BitconNodeAPI
+    extend self
+    ROOT = "http://blockchain.info/"
 
-  # addresses []string
-  def multi_addr(addresses)
+    # string address
+    # response hash or nil
+    def get_addr(address)
+        res = get("address/" + address + "?format=json")
+        return nil if error?(res)
+        JSON.parse(res.body)
+    end
 
-  end
+    # []string addresses
+    # response hash or nil
+    def multi_addr(addresses)
+        res = get("multi_addr?active="+addresses.join("|"))
+        return nil if error?(res)
+        JSON.parse(res.body)
+    end
 
+    def get(url)
+        HTTParty.get(ROOT + url)
+    end
+
+    # Have to do this cos blockchain info API is crappy
+    def error?(res)
+        res.code >= 400 || JSON.parse(res.body)["error"]
+    end
+    
 end
