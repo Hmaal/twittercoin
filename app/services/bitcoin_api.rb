2333773262fcf67e437903c8af31a7d10c5a6e96
sidazhang <sidazhang89@gmail.com>
2013-12-21 18:22:38 -0800

    Bitcoin Tx Building

--- app/services/bitcoin_api.rb
+++ app/services/bitcoin_api.rb
@@ -1,5 +1,6 @@
 module BitcoinAPI
   extend self
+  extend Bitcoin::Builder
 
   def generate_address()
     private_key, public_key = Bitcoin::generate_key
@@ -14,10 +15,82 @@ module BitcoinAPI
 
   # string address
   def get_info(address)
-    BitconNodeAPI.get_addr(address)
+    BitcoinNodeAPI.get_addr(address)
   end
 
-  def tx(from_address, to_address, amount)
+  # obj address, int amount
+  # returns httparty response obj
+  # exception is raised, ensure to rescue
+  # BitcoinAPI.send_tx(from_address, "15TLNJ24UFixU1Vn7eogJYvgaH324SocK4", 0.001.to_satoshi, FEE)
+  def send_tx(from_address, to_address, amount, fee)
+    raise AmountShouldBeFixnum if amount.class != Fixnum
+    hex, tx_hash = construct_tx(from_address, to_address, amount, fee)
+    push_tx(hex, tx_hash)
+  end
 
+  def construct_tx(from_address, to_address, amount, fee)
+    unspents = get_unspents(from_address.address, amount + fee)
+    prikey = from_address.decrypt()
+    key = Bitcoin::Key.from_base58(prikey)
+
+    new_tx = build_tx do |t|
+      unspents.each do |unspent|
+        t.input do |i|
+          i.prev_out(Bitcoin::P::Tx.from_hash(BitcoinNodeAPI.get_tx(unspent["tx_hash"].reverse_hex)))
+          i.prev_out_index(unspent["tx_output_n"])
+          i.signature_key(key)
+        end
+      end
+
+      t.output do |o|
+        o.value(amount)
+        o.script {|s| s.recipient(to_address) }
+      end
+
+      t.output do |o|
+        o.value(unspents.unspent_value - (amount+fee))
+        o.script {|s| s.recipient key.addr }
+      end
+    end
+
+    return new_tx.payload.unpack("H*").first, new_tx.hash
+  end
+
+  def get_unspents(from_address, total_value)
+    unspents = BitcoinNodeAPI.unspent([from_address])
+    select_unspents(unspents, total_value)
+  end
+
+  def select_unspents(unspents, total_value)
+    raise InsufficientAmount.new("Needed #{total_value}, but only had #{unspents.unspent_value}") if unspents.unspent_value < (total_value)
+    selected_unspents = []
+    unspents.each do |unspent|
+      if unspent["value"] >= total_value
+        return [unspent]
+      else
+        selected_unspents << unspent
+        return selected_unspents if selected_unspents.unspent_value >= total_value
+      end
+    end
+  end
+
+  def push_tx(hex, tx_hash)
+    BitcoinNodeAPI.push_tx(hex, tx_hash)
+  end
+
+  class TxError < StandardError; end
+  class InsufficientAmount < TxError; end
+  class AmountShouldBeInteger < TxError; end
+
+  class ::Array
+    def unspent_value
+      self.sum { |unspent| unspent["value"] }
+    end
+  end
+
+  class ::String
+    def reverse_hex
+      [self].pack('H*').reverse.unpack("H*").first
+    end
   end
 end
--- app/services/bitcoin_node_api.rb
+++ app/services/bitcoin_node_api.rb
@@ -1,30 +1,57 @@
-module BitconNodeAPI
+module BitcoinNodeAPI
     extend self
     ROOT = "http://blockchain.info/"
 
     # string address
     # response hash or nil
     def get_addr(address)
-        res = get("address/" + address + "?format=json")
-        return nil if error?(res)
+        get("address/" + address + "?format=json")
+    end
+
+    # string[] addresses
+    # response hash or nil
+    def multi_addr(addresses)
+        get("multi_addr?active="+addresses.join("|"))
+    end
+
+    # string[] addresses
+    # unspents[] or nil
+    def unspent(addresses)
+        get("unspent?active="+addresses.join("|"))["unspent_outputs"]
+    end
+
+    def get_tx(tx_hash)
+        res = HTTParty.get "https://blockexplorer.com/rawtx/#{tx_hash}"
         JSON.parse(res.body)
     end
 
-    # []string addresses
-    # response hash or nil
-    def multi_addr(addresses)
-        res = get("multi_addr?active="+addresses.join("|"))
-        return nil if error?(res)
-        JSON.parse(res.body)
+    def push_tx(hex, tx_hash)
+        payload = {
+          format: "plain",
+          tx: hex,
+          hash: tx_hash,
+          cors: "true",
+        }
+        post("pushtx", payload)
     end
 
     def get(url)
-        HTTParty.get(ROOT + url)
+        res = HTTParty.get(ROOT + url)
+        return nil if error?(res)
+        JSON.parse(res.body)
+    end
+
+    # TODO construct error message
+    def post(url, payload)
+        options = {body: payload}
+        res = HTTParty.post(ROOT + url, options)
+        raise "TODO" if res.code >= 400
+        res.body
     end
 
     # Have to do this cos blockchain info API is crappy
     def error?(res)
         res.code >= 400 || JSON.parse(res.body)["error"]
     end
-    
+
 end
