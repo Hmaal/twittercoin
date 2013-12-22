bcf0f5666510f4d3e2d98aaa72325fc90d9ec633
Scott Li <scottli0101@gmail.com>
2013-12-22 00:09:13 -0800

    hacked around gay flash

--- app/assets/javascripts/controllers/profilesCtrl.js
+++ app/assets/javascripts/controllers/profilesCtrl.js
@@ -1,15 +1,16 @@
 'use strict'
 
 tcApp.controller( "profilesCtrl", function( $scope, $resource, $routeParams, $location ) {
+	$scope.shown = false
 
 	var Profile = $resource( "/api/profiles/:screenName" )
-
 	$scope.direct = $routeParams.direct
 
 	var profile = Profile.get( {
 		screenName: $routeParams.screenName
 	}, function( profile ) {
 		$scope.profile = profile;
+		$scope.shown = true
 	}, function( error ) {
 		$location.path( "/" )
 	} )
