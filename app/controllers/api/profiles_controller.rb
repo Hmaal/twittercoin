87d5f117e125797b63e38d14b8c3b46ddcd04ba1
Scott Li <scottli0101@gmail.com>
2013-12-21 21:21:34 -0800

    built more profile page

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -11,32 +11,44 @@ class Api::ProfilesController < ActionController::Base
     @user = User.find_profile(params[:screen_name])
     @twitter_user = TipperClient.search_user(params[:screen_name])
 
-
-
-    # TODO: Avoid repetitive calls
+    total_tips_given = 0
+    total_tips_received = 0
 
     @tips = @user.all_tips.map do |tip|
+
+      # Cached
+      sender = TipperClient.search_user(tip.sender.screen_name)
+      recipient = TipperClient.search_user(tip.recipient.screen_name)
+
+      t = Tweet::Parser.new(tip.content, tip.screen_name)
+
+      # Count tips
+      giving = sender[:screenName] == params["screen_name"]
+      total_tips_given += tip.transaction.amount if giving
+      total_tips_received += tip.transaction.amount if !giving
+
       {
         sender: {
-          screenName: tip.sender.screenName
-          avatarSmall: tip.sender.avatarSmall
+          screenName: sender[:screenName],
+          avatarSmall: sender[:avatarSmall],
+          css: giving ? "yellow" : "default"
         },
-        receiver: {
-          screenName: tip.receiver.screenName,
-          avatarSmall: tip.receiver.avatarSmall
+        recipient: {
+          screenName: recipient[:screenName],
+          avatarSmall: recipient[:avatarSmall],
+          css: giving ? "default" : "yellow"
         },
-        txHash: tip.transaction.tx_hash,
+        txDirection:  giving ? "primary" : "success",
+        txHash: tip.transaction.try(:tx_hash),
         tweetLink: tip.build_link,
-        amount: tip.transaction.satoshis / SATOSHIS,
+        amount: tip.transaction.try(:satoshis),
         other: {
           presence: true,
           amount: 1,
-          unit: "Beer"
+          unit: "beer"
         }
       }
     end
-
-
 
     @profile = {
       screenName: @twitter_user[:screenName],
@@ -44,28 +56,9 @@ class Api::ProfilesController < ActionController::Base
       avatarLarge: @twitter_user[:avatarLarge],
       uid: @user.uid,
       authenticated: @user.authenticated,
-      totalTipsGiven: 0.011,
-      totalTipsReceived: 1.14,
-      tips: [
-        {
-          sender: {
-            screenName: 1,
-            avatarSmall: 1
-          },
-          receiver: {
-            screenName: 1,
-            avatarSmall: 1
-          },
-          txHash: 1,
-          tweetLink: 1,
-          amount: 0.001,
-          other: {
-            presence: true,
-            amount: 1,
-            unit: "Beer"
-          }
-        }
-      ]
+      totalTipsGiven: total_tips_given,
+      totalTipsReceived: total_tips_received,
+      tips: @tips
     }
 
 
