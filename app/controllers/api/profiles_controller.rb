b7c0e68cbc23b7304adbeef480e1bbfc5a6c1d94
Scott Li <scottli0101@gmail.com>
2013-12-21 21:55:37 -0800

    direct flash message

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,18 +1,15 @@
 class Api::ProfilesController < ActionController::Base
 
-  # TODO Authentication
-
-  def show
-    # TODO: Rails.cache.fetch
+  def build
 
     # TODO: No user?
+    @screen_name = params[:screen_name] || User.find_by(slug: session[:slug]).screen_name
 
+    @user = User.find_profile(@screen_name)
+    @twitter_user = TipperClient.search_user(@screen_name)
 
-    @user = User.find_profile(params[:screen_name])
-    @twitter_user = TipperClient.search_user(params[:screen_name])
-
-    total_tips_given = 0
-    total_tips_received = 0
+    total_satoshis_given = 0
+    total_satoshis_received = 0
 
     @tips = @user.all_tips.map do |tip|
 
@@ -24,8 +21,8 @@ class Api::ProfilesController < ActionController::Base
 
       # Count tips
       giving = sender[:screenName] == params["screen_name"]
-      total_tips_given += tip.transaction.amount if giving
-      total_tips_received += tip.transaction.amount if !giving
+      total_satoshis_given += tip.transaction.amount if giving
+      total_satoshis_received += tip.transaction.amount if !giving
 
       {
         sender: {
@@ -56,8 +53,9 @@ class Api::ProfilesController < ActionController::Base
       avatarLarge: @twitter_user[:avatarLarge],
       uid: @user.uid,
       authenticated: @user.authenticated,
-      totalTipsGiven: total_tips_given,
-      totalTipsReceived: total_tips_received,
+      totalTipsGiven: total_satoshis_given / SATOSHIS.to_f,
+      totalTipsReceived: total_satoshis_received / SATOSHIS.to_f,
+      publicKey: @user.addresses.first.public_key,
       tips: @tips
     }
 
