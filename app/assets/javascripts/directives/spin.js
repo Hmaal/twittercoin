1e4ebf6d6384cf9054bb042bfa229464151e352f
Scott Li <scottli0101@gmail.com>
2013-12-22 16:17:01 -0800

    added spinner and float issues

--- app/assets/javascripts/directives/spin.js
+++ app/assets/javascripts/directives/spin.js
@@ -3,11 +3,9 @@
 tcApp.directive( "spin", function() {
 	return {
 		restrict: "E",
-		scope: {
-			waiting: "="
-		},
 		link: function( $scope, element, attrs ) {
-			var opts = {
+			var opts;
+			var normal = {
 				lines: 17,
 				length: 40,
 				width: 4,
@@ -25,6 +23,32 @@ tcApp.directive( "spin", function() {
 				top: '200%',
 				left: '0%'
 			};
+
+			var small = {
+				lines: 13,
+				length: 10,
+				width: 2,
+				radius: 5,
+				corners: 1,
+				rotate: 0,
+				direction: 1,
+				color: '#000',
+				speed: 1,
+				trail: 60,
+				shadow: false,
+				hwaccel: false,
+				className: 'spinner',
+				zIndex: 2e9,
+				top: '10%',
+				left: '0%'
+			}
+
+			if ( attrs.size === "small" ) {
+				opts = small
+			} else {
+				opts = normal
+			}
+
 			var spinner = new Spinner( opts )
 			spinner.spin( element[ 0 ] );
 		}
