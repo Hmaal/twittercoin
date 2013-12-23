191711fcb41c3bea4b49d4fd28415a27d3f1380d
Scott Li <scottli0101@gmail.com>
2013-12-23 09:37:10 -0800

    reduced space

--- app/services/tweet/message.rb
+++ app/services/tweet/message.rb
@@ -5,7 +5,7 @@ module Tweet::Message
 
     def recipient(recipient, sender, amount)
       link = "tippercoin.com/#/profile/#{recipient}?direct=true&r=#{rand}"
-      "Hi @#{recipient}, @#{sender} just tipped you #{amount / SATOSHIS.to_f} BTC! "\
+      "@#{recipient}, @#{sender} just tipped you #{amount / SATOSHIS.to_f} BTC! "\
       "See it here #{link}"
     end
 
@@ -19,7 +19,7 @@ module Tweet::Message
 
     def unauthenticated(sender)
       link = "tippercoin.com/auth/twitter?r=#{rand}"
-      "Hey there @#{sender}, to start tipping, please authenticate via twitter "\
+      "@#{sender}, to start tipping, please authenticate via twitter "\
       "and make a deposit. Thanks! #{link}"
     end
 
@@ -27,32 +27,32 @@ module Tweet::Message
       # TODO: Include link with amount
 
       link = "tippercoin.com/auth/twitter?r=#{rand}"
-      "Hi @#{sender}, please top up on your account before sending this tip. #{link}"
+      "@#{sender}, please top up on your account before sending this tip. #{link}"
     end
 
     def enough_confirmed_unspents(sender)
       link = "tippercoin.com/#/account/deposit?r=#{rand}"
-      "Hi @#{sender}, you don't have enough confirmed unspents, pls wait for a few mins! #{link}"
+      "@#{sender}, you don't have enough confirmed unspents, pls wait for a few mins! #{link}"
     end
 
     def negative_amount(sender)
       link = "tippercoin.com/#/documentation?r=#{rand}"
-      "Hi @#{sender}, You can't send negative amounts! #{link}"
+      "@#{sender}, You can't send negative amounts! #{link}"
     end
 
     def zero_amount(sender)
       link = "tippercoin.com/#/how-it-works?r=#{rand}"
-      "Hi @#{sender}, please tip 0.001 BTC or more. Refer to #{link}"
+      "@#{sender}, please tip 0.001 BTC or more. Refer to #{link}"
     end
 
     def direct_tweet(sender)
       link = "tippercoin.com/#/how-it-works?r=#{rand}"
-      "Hi @#{sender}, please try tipping someone else. Refer to #{link}"
+      "@#{sender}, please try tipping someone else. Refer to #{link}"
     end
 
     def unknown(sender)
       link = "tippercoin.com/#/how-it-works?r=#{rand}"
-      "Hi @#{sender}, sorry, I'm not sure what you meant :s. Please refer to #{link}"
+      "@#{sender}, sorry, I'm not sure what you meant :s. Please refer to #{link}"
     end
 
     def rand(limit: 2)
