2175fb5b1ff550b3470f911f3f082a6656932f2a
Scott Li <scottli0101@gmail.com>
2013-12-22 17:47:58 -0800

    debug log levels

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,8 @@ Twittercoin::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = 'DEBUG'
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
